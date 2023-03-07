21f79cb4068020dfc3477cc54c9af108a33d6732
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-07 11:38:06 -0300

    🚧
    accomplished the challenge 4 and 5 created
    
      4 - a new bank related to the previously created
    
      5 - I made a post in the new table

--- script.ts
+++ script.ts
@@ -4,10 +4,29 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  const resultado = await prisma.user.create({
-    data: { email: 'matheus@TextTrackList.com' }
+  // ? Revelar a tabela interira
+  // const resultado1 = await prisma.user.findMany()
+
+  // ? criar um novo usuario
+  // const resultado2 = await prisma.user.create({
+  //   data: { email: 'Bob@text.com', name: 'bob' }
+  // })
+
+  // ? Atualizar o usuario anterior sem nome
+  // const resultado3 = await prisma.user.update({
+  //   data: { name: 'matheus' },
+  //   where: {
+  //     id: 4
+  //   }
+  // })
+
+  const resultado4 = await prisma.post.create({
+    data: {
+      title: 'Hello World'
+    }
   })
-  console.log(resultado)
+
+  console.log(resultado4)
 }
 
 main()
