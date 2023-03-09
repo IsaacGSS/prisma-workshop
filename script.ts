baffa1d45a460e37749933396b07b2d72373675c
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-09 08:24:06 -0300

    🚧
    add tarefas 7,8,9,10,11,12

--- script.ts
+++ script.ts
@@ -41,7 +41,59 @@ async function main() {
   //   }
   // })
 
-  console.log(resultado7)
+  // ? Escreva uma consulta para buscar um único registro da tabela User pelo email
+  // const resultado7 = await prisma.user.findUnique({
+  //   where: { id: 6
+  //   }
+  // })
+
+  // const resultado7 = await prisma.user.findMany({
+  //   select: { id: true, email: true }
+  // })
+
+  // const resultado7 = await prisma.user.findMany()
+
+  // console.log(resultado7[5].email)
+
+  // ? Escreva uma consulta que selecione apenas alguns campos
+  // const resultado8 = await prisma.user.findMany({
+  //   select: { id: true, name: true }
+  // })
+
+  // const resultado9 = await prisma.user.findMany({
+  //   where: { id: 7 },
+  //   include: {
+  //     Post: true
+  //   }
+  // })
+
+  // const resultado10 = await prisma.user.create({
+  //   data: {
+  //     email: 'Alexandre@text.com',
+  //     name: 'Alexandre',
+  //     Post: {
+  //       create: {
+  //         title: 'post',
+  //         content: 'hgsjhghjdsag'
+  //       }
+  //     }
+  //   }
+  // })
+
+  // const resultado11 = await prisma.user.findMany({
+  //   where: {
+  //     name: {
+  //       startsWith: 'm'
+  //     }
+  //   }
+  // })
+
+  // const resultado12 = await prisma.user.findMany({
+  //   skip: 0,
+  //   take: 10
+  // })
+
+  console.log(resultado12)
 }
 
 main()
